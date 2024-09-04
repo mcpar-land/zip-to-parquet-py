5a7ac3bbfef108add693759bd153b591f39bcdf1
mcpar-land <john@mcpar.land>
2024-09-04 13:44:48 -0400

    initial commit

---
+++ devenv.nix
@@ -0,0 +1,15 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}: {
+  languages.nix.enable = true;
+  languages.python.enable = true;
+  languages.python.venv.enable = true;
+  languages.python.venv.quiet = true;
+  languages.python.venv.requirements = ''
+    maturin
+  '';
+}
