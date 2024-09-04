b3ffa702f0c0323faa9cafcb4ccdb43c1d49710d
mcpar-land <john@mcpar.land>
2024-09-04 13:49:15 -0400

    correctly name module to zip_to_parquet

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 
 /// A Python module implemented in Rust.
 #[pymodule]
-fn zip_to_parquet_py(m: &Bound<'_, PyModule>) -> PyResult<()> {
+fn zip_to_parquet(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     Ok(())
 }
